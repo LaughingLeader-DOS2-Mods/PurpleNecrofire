3f04722c3d4ffb59c22aacd8bc41379322d5d5e2
LaughingLeader <laughingleader@gmail.com>
2020-04-16 02:35:46 -0500

    Updated to extender v42

--- Mods/ZZZ_PurpleNecrofire_c46b8710-e5f5-45f5-9485-a3993e11c951/Story/RawFiles/Lua/BootstrapClient.lua
+++ Mods/ZZZ_PurpleNecrofire_c46b8710-e5f5-45f5-9485-a3993e11c951/Story/RawFiles/Lua/BootstrapClient.lua
@@ -105,7 +105,7 @@ local function get_stat_override(statname)
     return nil
 end
 
-local ModuleLoad = function ()
+local function ModuleSetup()
     Ext.Print("[ZZZ_PurpleNecrofire:Bootstrap.lua] Module is loading.")
 
     --Larian's Pet Power Mod
@@ -216,64 +216,4 @@ local ModuleLoad = function ()
     Ext.Print("[LLPURPLEFIRE:Bootstrap.lua] Changed ("..tostring(total_changes)..") properties in ("..tostring(total_stats)..") stats to use the new effects/icons/names.")
 end
 
-local function LLPURPLEFIRE_ModUpdated(past_version,new_version)
-    -- local version_is_less_than = _G["LeaderLib_Ext_VersionIsLessThan"]
-    -- if version_is_less_than ~= nil then
-    --     if version_is_less_than(past_version, 1,1,0,0) == true then
-            
-    --     end
-    -- end
-end
-
-function LLPURPLEFIRE_Ext_Debug()
-    local character = CharacterGetHostCharacter()
-    if ObjectGetFlag(character, "LLPURPLEFIRE_DebugSet") == 0 then
-        --CharacterLevelUpTo(character, 11);
-        --CharacterApplyPreset(character, "Wizard_Act2");
-        CharacterAddSkill(character, "Projectile_InfectiousFlame");
-        CharacterAddSkill(character, "Summon_Incarnate");
-        CharacterAddSkill(character, "Target_NecrofireInfusion");
-        --CharacterAddSkill(character, "Projectile_EnemyInfectiousFlame_Ooze");
-        --CharacterAddSkill(character, "Projectile_EnemyInfectiousFlame_Adrama");
-        --CharacterAddSkill(character, "Projectile_EnemyFireball_Cursed");
-        --CharacterAddSkill(character, "Projectile_EnemyFireball_Cursed_Insect");
-        CharacterAddSkill(character, "Zone_EnemyLaserRayCursed");
-        --CharacterAddSkill(character, "Projectile_IncarnateFireball");
-        CharacterAddAbility(character, "FireSpecialist", 8);
-        CharacterAddAbility(character, "EarthSpecialist", 8);
-        CharacterAddAbility(character, "Summoning", 10);
-        CharacterAddAttribute(character, "Memory", 10);
-        --ObjectSetFlag(character, "FTJ_RemoveSourceCollar", 0);
-        --CharacterOverrideMaxSourcePoints(character, 12);
-        --CharacterAddSourcePoints(character, 12);
-        ObjectSetFlag(character, "LLPURPLEFIRE_DebugSet", 0);
-    end
-    PartyAddGold(character, 30000)
-end
-
-local function SessionLoading()
-    -- if _G["LeaderLib_Ext_RegisterMod"] ~= nil then
-    --     local func = _G["LeaderLib_Ext_RegisterMod"]
-    --     func("PurpleNecrofire", "LaughingLeader", 1,0,0,0, "c46b8710-e5f5-45f5-9485-a3993e11c951")
-    -- end
-
-    if _G["LeaderLib_ModUpdater"] ~= nil then
-        local update_table = _G["LeaderLib_ModUpdater"]
-        update_table["c46b8710-e5f5-45f5-9485-a3993e11c951"] = LLPURPLEFIRE_ModUpdated
-    end
-
-    if _G["LeaderLib_DebugInitCalls"] ~= nil then
-        local debug_table = _G["LeaderLib_DebugInitCalls"]
-        debug_table[#debug_table+1]= LLPURPLEFIRE_Ext_Debug
-    end
-end
-
---v36 and higher
-if Ext.RegisterListener ~= nil then
-    Ext.RegisterListener("SessionLoading", SessionLoading)
-    Ext.RegisterListener("ModuleLoading", ModuleLoad)
-else
-    Ext.Print("[LLPURPLEFIRE:Bootstrap.lua] [*WARNING*] Extender version is less than v36! Stat overrides ain't happenin', chief.")
-end
-
-Ext.Print("[ZZZ_PurpleNecrofire:Bootstrap.lua] Finished running.")
+Ext.RegisterListener("ModuleLoading", ModuleSetup)
